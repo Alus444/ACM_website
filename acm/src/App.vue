b9c430d9056a410ab641ed637d6ea278c49ca258
Alus <alus0000stairs@gmail.com>
2026-07-26 12:31:42 +0900

    NIGHTOVER機能リファレンスを追加

--- acm/src/App.vue
+++ acm/src/App.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 import Header from './components/layout/Header.vue'
 import Footer from './components/layout/Footer.vue'
 import UnderConstruction from './components/sections/UnderConstruction.vue'
 
 // ★ true = 工事中ページ / false = 通常ページ
 const underConstruction = false
+const route = useRoute()
+const isStandalone = computed(() => route.meta.standalone === true)
 </script>
 
 <template>
   <UnderConstruction v-if="underConstruction" />
+  <RouterView v-else-if="isStandalone" />
   <template v-else>
     <Header />
     <RouterView />
